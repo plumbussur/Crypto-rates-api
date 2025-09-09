e4f003e8a305bb38cb09a67aff0d292cfb8e4d9c
Stepan Dorofeew <stepadorofeew@yandex.ru>
2025-09-09 03:22:12 +0300

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import CryptocurrencyCard from 'D:/python/Project/Crypto/frontend/src/components/cryptocurrencyCard.jsx'
+
+
+function App() {
+  
+  return (
+    <div>
+      <CryptocurrencyCard/>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/cryptocurrencyCard.jsx
@@ -0,0 +1,12 @@
+function CryptocurrencyCard() {
+  
+  return (
+    <>
+      <div>
+        <h1>hello world</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
